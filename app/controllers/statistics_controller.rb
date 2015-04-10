6005d12738eac2ffa537f41b1e70ba759c71ec05
Stephen <stemitchell91@hotmail.co.uk>
2015-04-10 10:00:42 +0000

    views now actually work

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -13,77 +13,6 @@ def index
 	 end
 end
   
-def import
-	Statistic.import(params[:file])
-	redirect_to statistics_path, notice: "Statistics imported."
-end
-
-  # GET /statistics/1
-  # GET /statistics/1.json
-  def show
-  end
-
-  # GET /statistics/new
-  def new
-    @statistic = Statistic.new
-  end
-
-  # GET /statistics/1/edit
-  def edit
-  end
-
-  # POST /statistics
-  # POST /statistics.json
-  def create
-    @statistic = Statistic.new(statistic_params)
-
-    respond_to do |format|
-      if @statistic.save
-        format.html { redirect_to @statistic, notice: 'Statistic was successfully created.' }
-        format.json { render :show, status: :created, location: @statistic }
-      else
-        format.html { render :new }
-        format.json { render json: @statistic.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /statistics/1
-  # PATCH/PUT /statistics/1.json
-  def update
-    respond_to do |format|
-      if @statistic.update(statistic_params)
-        format.html { redirect_to @statistic, notice: 'Statistic was successfully updated.' }
-        format.json { render :show, status: :ok, location: @statistic }
-      else
-        format.html { render :edit }
-        format.json { render json: @statistic.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /statistics/1
-  # DELETE /statistics/1.json
-  def destroy
-    @statistic.destroy
-    respond_to do |format|
-      format.html { redirect_to statistics_url, notice: 'Statistic was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_statistic
-      @statistic = Statistic.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def statistic_params
-      params.require(:statistic).permit(:year, :number1, :number2, :number3, :number4, :number5, :number6, :number7, :number8, :name)
-    end
-end
-
 def victimisation
   @statistics = Statistic.all
 end
@@ -174,4 +103,75 @@ end
 
 def volume
   @statistics = Statistic.all
+end  
+  
+def import
+	Statistic.import(params[:file])
+	redirect_to statistics_path, notice: "Statistics imported."
+end
+
+  # GET /statistics/1
+  # GET /statistics/1.json
+  def show
+  end
+
+  # GET /statistics/new
+  def new
+    @statistic = Statistic.new
+  end
+
+  # GET /statistics/1/edit
+  def edit
+  end
+
+  # POST /statistics
+  # POST /statistics.json
+  def create
+    @statistic = Statistic.new(statistic_params)
+
+    respond_to do |format|
+      if @statistic.save
+        format.html { redirect_to @statistic, notice: 'Statistic was successfully created.' }
+        format.json { render :show, status: :created, location: @statistic }
+      else
+        format.html { render :new }
+        format.json { render json: @statistic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /statistics/1
+  # PATCH/PUT /statistics/1.json
+  def update
+    respond_to do |format|
+      if @statistic.update(statistic_params)
+        format.html { redirect_to @statistic, notice: 'Statistic was successfully updated.' }
+        format.json { render :show, status: :ok, location: @statistic }
+      else
+        format.html { render :edit }
+        format.json { render json: @statistic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /statistics/1
+  # DELETE /statistics/1.json
+  def destroy
+    @statistic.destroy
+    respond_to do |format|
+      format.html { redirect_to statistics_url, notice: 'Statistic was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_statistic
+      @statistic = Statistic.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def statistic_params
+      params.require(:statistic).permit(:year, :number1, :number2, :number3, :number4, :number5, :number6, :number7, :number8, :name)
+    end
 end
